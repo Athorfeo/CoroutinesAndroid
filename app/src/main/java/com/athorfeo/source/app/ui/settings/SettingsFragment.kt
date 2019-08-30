7311c01d755cccd1a88f800bf9d6f8c1d6f82cba
Juan D. Ortiz <juanortiz@periferia-it.com>
2019-08-30 10:35:29 -0500

    - Se modifica los settings para el lenguaje de la app. - Se implementa en los diálogos el SingleEvent para que no muestre dialogos si no solo cuando se ejecuten.

--- app/src/main/java/com/athorfeo/source/app/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/athorfeo/source/app/ui/settings/SettingsFragment.kt
@@ -1,12 +1,24 @@
 package com.athorfeo.source.app.ui.settings
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.preference.PreferenceFragmentCompat
 import com.athorfeo.source.R
 import com.athorfeo.source.di.Injectable
 
 class SettingsFragment: PreferenceFragmentCompat(), Injectable {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        menu.clear()
+    }
+
 }
--- app/src/main/java/com/athorfeo/source/app/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/athorfeo/source/app/viewmodel/BaseViewModel.kt
@@ -6,20 +6,18 @@ import androidx.lifecycle.ViewModel
 import com.athorfeo.source.app.model.ErrorResource
 import com.athorfeo.source.utility.Status
 import com.athorfeo.source.app.model.Resource
+import com.athorfeo.source.utility.SingleLiveEvent
 
 
 open class BaseViewModel: ViewModel() {
-    private val _isLoading: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>().apply { value = false } }
-    val isLoading: LiveData<Boolean> by lazy{ _isLoading }
+    val isLoading: SingleLiveEvent<Boolean> by lazy { SingleLiveEvent<Boolean>().apply { value = false } }
+    val isError: SingleLiveEvent<ErrorResource> by lazy { SingleLiveEvent<ErrorResource>()}
 
-    private val _isError: MutableLiveData<ErrorResource> by lazy { MutableLiveData<ErrorResource>()}
-    val isError: LiveData<ErrorResource> by lazy{ _isError }
+    private fun setLoading(boolean: Boolean){ isLoading.value = boolean }
+    private fun postLoading(boolean: Boolean){ isLoading.postValue(boolean) }
 
-    private fun setLoading(boolean: Boolean){ _isLoading.value = boolean }
-    private fun postLoading(boolean: Boolean){ _isLoading.postValue(boolean) }
-
-    fun setError(code: Int?, message: String? = null){ _isError.value = ErrorResource(code, message) }
-    fun postError(code: Int?, message: String? = null){ _isError.postValue(ErrorResource(code, message)) }
+    fun setError(code: Int?, message: String? = null){ isError.value = ErrorResource(code, message) }
+    fun postError(code: Int?, message: String? = null){ isError.postValue(ErrorResource(code, message)) }
 
     /**
      * Resource Handler
