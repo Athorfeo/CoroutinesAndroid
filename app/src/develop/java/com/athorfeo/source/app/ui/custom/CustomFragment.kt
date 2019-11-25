7a2b1f6516f010453ba7867494342774be3254bf
Juan Ortiz <jortiz@novopayment.com>
2019-11-25 13:32:08 -0500

    Se agrega libreria y referencias

--- app/src/develop/java/com/athorfeo/source/app/ui/custom/CustomFragment.kt
+++ app/src/develop/java/com/athorfeo/source/app/ui/custom/CustomFragment.kt
@@ -1,34 +1,15 @@
 package com.athorfeo.source.app.ui.custom
-
-import android.app.FragmentManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.athorfeo.source.BuildConfig
 import com.athorfeo.source.R
-import com.athorfeo.source.api.response.SearchMoviesResponse
 import com.athorfeo.source.app.ui.base.fragment.BaseFragment
 import com.athorfeo.source.databinding.FragmentCustomBinding
-import com.athorfeo.source.databinding.FragmentMainBinding
 import com.athorfeo.source.testing.OpenForTesting
-import com.athorfeo.source.util.ResponseCode
-import com.athorfeo.source.util.findFieldAnnotation
 import com.athorfeo.source.util.ui.CustomDialogFragment
-import com.athorfeo.source.util.ui.DialogUtil
-import com.google.gson.annotations.SerializedName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import org.jetbrains.annotations.TestOnly
-import timber.log.Timber
-import javax.inject.Inject
 
 /**
  * Fragmento que lista las peliculas
