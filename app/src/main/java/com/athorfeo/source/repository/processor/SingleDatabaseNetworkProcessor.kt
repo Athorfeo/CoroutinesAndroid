07c88fd39a9df3a4375aef42a07b77b55925c443
Juan Ortiz <jortiz@novopayment.com>
2019-11-26 09:17:29 -0500

    mejora en coroutines

--- app/src/main/java/com/athorfeo/source/repository/processor/SingleDatabaseNetworkProcessor.kt
+++ app/src/main/java/com/athorfeo/source/repository/processor/SingleDatabaseNetworkProcessor.kt
@@ -22,13 +22,12 @@ import timber.log.Timber
  * @date 28/09/2019
  */
 abstract class SingleDatabaseNetworkProcessor <ResultType, DataType, ResponseType>{
-    private val job = SupervisorJob()
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + job)
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
     private val result = SingleLiveEvent<Resource<ResultType>>()
 
     init {
         result.value = Resource.loading(null)
-        coroutineScope.launch(Dispatchers.IO){
+        coroutineScope.launch{
             process()
         }
     }
@@ -43,56 +42,59 @@ abstract class SingleDatabaseNetworkProcessor <ResultType, DataType, ResponseTyp
     }
 
     private suspend fun process(){
-        try {
-            //1. Obtener valores de la db
-            Timber.i("Obtenemos los valores de la DB")
-            var dbSource = loadData()
+        withContext(Dispatchers.IO){
+            try {
+                //1. Obtener valores de la db
+                Timber.i("Obtenemos los valores de la DB")
+                var dbSource = loadData()
 
-            //2. Guardar en la db
-            Timber.i("Guardamos los datos en la DB")
-            val dbResponse = saveData()
+                //2. Guardar en la db
+                Timber.i("Guardamos los datos en la DB")
+                val dbResponse = saveData()
 
-            Timber.i("Validamos si es correcto el query")
-            if(isValidQuery(dbResponse)){
-                Timber.i("Query correcto, se obtienen los nuevos valores")
-                dbSource = loadData()
-            }else{ Timber.i("Query Incorrecto") }
+                Timber.i("Validamos si es correcto el query")
+                if(isValidQuery(dbResponse)){
+                    Timber.i("Query correcto, se obtienen los nuevos valores")
+                    dbSource = loadData()
+                }else{ Timber.i("Query Incorrecto") }
 
-            //3. Ejecutamos el servicio
-            if(dbSource != null){
-                Timber.i("Ejecutamos el servicio")
-                val response = service(dbSource)
-                val apiResponse = ApiResponse.create(response)
-                val isCorrect = processApiResponse(apiResponse)
+                //3. Ejecutamos el servicio
+                if(dbSource != null){
+                    Timber.i("Ejecutamos el servicio")
+                    val response = service(dbSource)
+                    val apiResponse = ApiResponse.create(response)
+                    val isCorrect = processApiResponse(apiResponse)
 
-                Timber.i("Validamos si es correcto el llamado al servicio")
-                if(isCorrect){
-                    Timber.i("Es correcto el servicio")
-                    setValue(Resource.success(dbResponse, onSuccessCode()))
+                    Timber.i("Validamos si es correcto el llamado al servicio")
+                    if(isCorrect){
+                        Timber.i("Es correcto el servicio")
+                        setValue(Resource.success(dbResponse, onSuccessCode()))
+                    }else{
+                        Timber.i("Es error el servicio")
+                        setValue(
+                            Resource.error(
+                                dbResponse,
+                                onErrorCode(),
+                                "Mensaje"
+                            )
+                        )
+                    }
                 }else{
-                    Timber.i("Es error el servicio")
+                    throw QueryDatabaseException("El resultado de la consulta no fue válido")
+                }
+            }catch(exception: Exception){
+                with(exception){
+                    Timber.e(this)
                     setValue(
-                        Resource.error<ResultType>(
-                            dbResponse,
-                            onErrorCode(),
-                            "Mensaje"
+                        Resource.error(
+                            null,
+                            getCode(onErrorCode()),
+                            localizedMessage
                         )
                     )
                 }
-            }else{
-                throw QueryDatabaseException("El resultado de la consulta no fue válido")
             }
-        }catch(exception: Exception){
-            with(exception){
-                Timber.e(this)
-                setValue(
-                    Resource.error(
-                        null,
-                        getCode(onErrorCode()),
-                        localizedMessage
-                    )
-                )
-            }
+            cancel()
         }
     }
     private fun processApiResponse(apiResponse: ApiResponse<ResponseType>) : Boolean =
