ff4d1f4d87a06c59601e0c6e718375487db57b9f
Juan Ortiz <jortiz@novopayment.com>
2019-12-02 14:08:43 -0500

    Funciones de libreria

--- utilities/src/main/java/com/athorfeo/utilities/User.kt
+++ utilities/src/main/java/com/athorfeo/utilities/User.kt
@@ -1,3 +1,18 @@
 package com.athorfeo.utilities
 
-data class User(val name: String){}
+import android.util.Log
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+data class User(val name: String){
+    fun log(){
+        CoroutineScope(Dispatchers.Main).launch{
+            withContext(Dispatchers.Default){
+                Log.i("UserLog", Gson().toJson(this@User))
+            }
+        }
+    }
+}
